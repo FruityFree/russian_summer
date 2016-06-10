d79f9f3daa45da2b861d5c046f67e6c7283552ce
Kirill <kirill.fedyanin@gmail.com>
2016-06-10 17:04:20 +0300

    shuffle!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+var intervalCount;
+var intervalId;
+var lastOutfitNum;
+
 function launchAnimation(){
+  intervalCount = 0;
+  $("#outfit").show();
+  intervalId = setInterval(changeOutfit, 150);
+  changeOutfit();
+
+}
+function changeOutfit(){
+  intervalCount += 1;
+  if (intervalCount > 20){
+    clearInterval(intervalId);
+  }
+  console.log(intervalCount);
   var outfitNum = Math.ceil(5*Math.random());
   var src = "/img/outfits/"+outfitNum+".png";
-  console.log(src);
-  $("#outfit").show();
   $("#outfit-img").attr("src", src);
 }
 
