d81c61e0ae7ee5feff3012ae1243fe48fa33d0d8
Kirill <kirill.fedyanin@gmail.com>
2016-06-10 16:47:32 +0300

    change the outfits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 function launchAnimation(){
-  
+  var outfitNum = Math.ceil(5*Math.random());
+  var src = "/img/outfits/"+outfitNum+".png";
+  console.log(src);
+  $("#outfit").show();
+  $("#outfit-img").attr("src", src);
 }
+
+launchAnimation();
