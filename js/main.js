e16922c750cf11133db9c9e8bcecb535329a423c
Kirill <kirill.fedyanin@gmail.com>
2016-06-10 17:11:50 +0300

    shuffle easing

--- js/main.js
+++ js/main.js
@@ -1,14 +1,20 @@
 var intervalCount;
 var intervalId;
 var lastOutfitNum;
+var intervalsMx = [2,1.9,1.75, 1.65, 1.6, 1.55, 1.5, 1.45, 1.4, 1.3, 1.2, 1.1, 1,1,1,1,1,1,1,1,1.2, 1.3, 1.4, 1.5, 1.8, 2]
+var basicTimeout = 120;
 
 function launchAnimation(){
   intervalCount = 0;
   $("#outfit").show();
-  intervalId = setInterval(changeOutfit, 150);
+  intervalId = setInterval(changeOutfit, interval(intervalCount));
   changeOutfit();
-
 }
+
+function interval(count){
+  return intervalsMx[count]*basicTimeout;
+}
+
 function changeOutfit(){
   intervalCount += 1;
   if (intervalCount > 20){
