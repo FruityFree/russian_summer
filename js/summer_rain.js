f502207dd24a0cf9d57592954072e19064eba518
Kirill <kirill.fedyanin@gmail.com>
2016-06-11 15:25:14 +0300

    animated circle

--- js/summer_rain.js
+++ js/summer_rain.js
@@ -12,6 +12,13 @@ var startTheRain;
     var canvasWidth = mainCanvas.width;
     var canvasHeight = mainCanvas.height;
 
+    var requestAnimationFrame = window.requestAnimationFrame ||
+                            window.mozRequestAnimationFrame ||
+                            window.webkitRequestAnimationFrame ||
+                            window.msRequestAnimationFrame;
+
+    var angle = 0;
+
     function drawCircle() {
       mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
 
@@ -23,12 +30,16 @@ var startTheRain;
          mainContext.beginPath();
 
          var radius = 175;
+         var radius = 25 + 150 * Math.abs(Math.cos(angle));
          mainContext.arc(225, 225, radius, 0, Math.PI * 2, false);
          mainContext.closePath();
 
          // color in the circle
          mainContext.fillStyle = "#006699";
          mainContext.fill();
+         angle += Math.PI / 164;
+
+         requestAnimationFrame(drawCircle);
     }
     drawCircle();
   }
