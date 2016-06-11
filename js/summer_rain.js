4b24d0b4b86eb48d30fcbd68e68e1bbc2e294f3d
Kirill <kirill.fedyanin@gmail.com>
2016-06-11 15:34:40 +0300

    multiple circles

--- js/summer_rain.js
+++ js/summer_rain.js
@@ -13,35 +13,73 @@ var startTheRain;
     var canvasHeight = mainCanvas.height;
 
     var requestAnimationFrame = window.requestAnimationFrame ||
-                            window.mozRequestAnimationFrame ||
-                            window.webkitRequestAnimationFrame ||
-                            window.msRequestAnimationFrame;
+      window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame ||
+      window.msRequestAnimationFrame;
 
-    var angle = 0;
+    var circles = []
 
-    function drawCircle() {
-      mainContext.clearRect(0, 0, canvasWidth, canvasHeight);
+    function Circle(radius, speed, width, xPos, yPos){
+      this.radius = radius;
+      this.speed = speed;
+      this.width = width;
+      this.xPos = xPos;
+      this.yPos = yPos;
+      this.opacity = 0.05 + Math.random() * 0.5;
 
-         // color in the background
-         mainContext.fillStyle = "#EEEEEE";
-         mainContext.fillRect(0, 0, canvasWidth, canvasHeight);
+      this.counter = 0;
 
-         // draw the circle
-         mainContext.beginPath();
+      var signHelper = Math.floor(Math.random() * 2);
 
-         var radius = 175;
-         var radius = 25 + 150 * Math.abs(Math.cos(angle));
-         mainContext.arc(225, 225, radius, 0, Math.PI * 2, false);
-         mainContext.closePath();
-
-         // color in the circle
-         mainContext.fillStyle = "#006699";
-         mainContext.fill();
-         angle += Math.PI / 164;
-
-         requestAnimationFrame(drawCircle);
+      if (signHelper == 1) {
+        this.sign = -1;
+      } else {
+        this.sign = 1;
+      }
     }
-    drawCircle();
+
+    Circle.prototype.update = function() {
+     this.counter += this.sign * this.speed;
+
+     mainContext.beginPath();
+     mainContext.arc(this.xPos + Math.cos(this.counter / 100) * this.radius,
+       this.yPos + Math.sin(this.counter / 100) * this.radius,
+       this.width,
+       0,
+       Math.PI * 2,
+       false);
+
+     mainContext.closePath();
+
+     mainContext.fillStyle = 'rgba(185, 211, 238,' + this.opacity + ')';
+     mainContext.fill();
+   };
+
+   function drawCircles() {
+    for (var i = 0; i < 100; i++) {
+      var randomX = Math.round(-200 + Math.random() * 700);
+      var randomY = Math.round(-200 + Math.random() * 700);
+      var speed = 0.2 + Math.random() * 3;
+      var size = 5 + Math.random() * 100;
+
+      var circle = new Circle(100, speed, size, randomX, randomY);
+      circles.push(circle);
+    }
+    draw();
+  }
+  drawCircles();
+
+  function draw() {
+    mainContext.clearRect(0, 0, 500, 500);
+
+    for (var i = 0; i < circles.length; i++) {
+      var myCircle = circles[i];
+      myCircle.update();
+    }
+    requestAnimationFrame(draw);
+  }
+
+
   }
 
 })();
