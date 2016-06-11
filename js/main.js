0b29c10db75c5c9d296bbdee6b03eb3e7e8b838a
Kirill <kirill.fedyanin@gmail.com>
2016-06-11 14:57:54 +0300

    Hide code from global namespace

--- js/main.js
+++ js/main.js
@@ -1,73 +1,80 @@
-var intervalCount;
-var intervalId;
-var outfitOverallCount = 4;
-var isCyclist = true;
+var launchAnimation;
+var changeCyclist;
 
-var basicTimeout = 280;
-var changesOverallNum = 20;
-// var intervalsMx = [2.5,2.2, 2, 1.9, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1,
-//   1.1,1, 1,1,1,1.2,1.5,1.7,2,3,4]
+(function(){
+  var intervalCount;
+  var intervalId;
+  var outfitOverallCount = 4;
+  var isCyclist = true;
+  // launchAnimation = launchAnimation;
 
 
-function launchAnimation(){
-  reset();
-  $(".outfit").show();
-  setTimeout(changeOutfit, interval(intervalCount));
-  // changeOutfit();
-}
+  var basicTimeout = 280;
+  var changesOverallNum = 20;
+  // var intervalsMx = [2.5,2.2, 2, 1.9, 1.6, 1.5, 1.4, 1.3, 1.2, 1.1,
+  //   1.1,1, 1,1,1,1.2,1.5,1.7,2,3,4]
 
-function interval(count){
-  return basicTimeout;
-}
 
-function changeOutfit(){
-  intervalCount += 1;
-  console.log(intervalCount);
-  if (intervalCount > changesOverallNum){
-    setTimeout(showSigns, 800);
-  } else {
+  function launchAnimation(){
+    reset();
+    $(".outfit").show();
     setTimeout(changeOutfit, interval(intervalCount));
+    // changeOutfit();
   }
 
-  changeItems();
-}
-
-function changeItems(){
-  changeItem("boots");
-  changeItem("pants");
-  changeItem("jacket");
-
-}
-function changeItem(name){
-  var num = Math.ceil(outfitOverallCount*Math.random())
-  $("#"+name+"-img").attr("src", "/img/outfits/"+name+num+".png");
-}
-
-
-function showSigns(){
-  $("#weather").show();
-  $("#change-cyclist").show();
-}
-
-function reset(){
-  intervalCount = 0;
-  $("#weather").hide();
-  $("#change-cyclist").hide();
-  $(".outfit").hide();
-}
-
-function changeCyclist(){
-  if (isCyclist){
-    $("#cyclist").attr("src", "/img/pedestrian.png");
-    $("#change-cyclist").text("хочу кататься");
-    isCyclist = false;
-  } else {
-    $("#cyclist").attr("src", "/img/cyclist_base.png");
-    $("#change-cyclist").text("не хочу кататься");
-    isCyclist = true;
+  function interval(count){
+    return basicTimeout;
   }
-  reset();
-}
 
-//TODO remove on production
-//launchAnimation();
+  function changeOutfit(){
+    intervalCount += 1;
+    console.log(intervalCount);
+    if (intervalCount > changesOverallNum){
+      setTimeout(showSigns, 800);
+    } else {
+      setTimeout(changeOutfit, interval(intervalCount));
+    }
+
+    changeItems();
+  }
+
+  function changeItems(){
+    changeItem("boots");
+    changeItem("pants");
+    changeItem("jacket");
+
+  }
+  function changeItem(name){
+    var num = Math.ceil(outfitOverallCount*Math.random())
+    $("#"+name+"-img").attr("src", "/img/outfits/"+name+num+".png");
+  }
+
+
+  function showSigns(){
+    $("#weather").show();
+    $("#change-cyclist").show();
+  }
+
+  function reset(){
+    intervalCount = 0;
+    $("#weather").hide();
+    $("#change-cyclist").hide();
+    $(".outfit").hide();
+  }
+
+  function changeCyclist(){
+    if (isCyclist){
+      $("#cyclist").attr("src", "/img/pedestrian.png");
+      $("#change-cyclist").text("хочу кататься");
+      isCyclist = false;
+    } else {
+      $("#cyclist").attr("src", "/img/cyclist_base.png");
+      $("#change-cyclist").text("не хочу кататься");
+      isCyclist = true;
+    }
+    reset();
+  }
+
+  //TODO remove on production
+  //launchAnimation();
+})()
