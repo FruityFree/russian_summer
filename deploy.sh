b6d57a8ceb143eff40c1b6ec1cba2c17df36ada3
Kirill <kirill.fedyanin@gmail.com>
2016-06-13 00:37:28 +0300

    kind a setupble script

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,8 @@
-ssh -t ponyo@188.166.18.121 "rm -rf /home/ponyo/apps/russian_summer/*"
-scp -r ./* ponyo@188.166.18.121:/home/ponyo/apps/russian_summer
+#!/bin/sh
+
+USER="ponyo"
+SERVER="188.166.18.121"
+DEPLOY_FOLDER="/home/ponyo/apps/russian_summer"
+
+ssh -t $USER@$SERVER "rm -rf $DEPLOY_FOLDER/*"
+scp -r ./* $USER@$SERVER:$DEPLOY_FOLDER
