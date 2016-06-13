c8222a4f245f909b09d906678c4fc408c957866f
Kirill <kirill.fedyanin@gmail.com>
2016-06-14 00:19:51 +0300

    weather for tomorrow, not today

--- js/weather.js
+++ js/weather.js
@@ -1,10 +1,10 @@
 var getTemperature;
 (function(){
-  var params = "id=524901&APPID=a5e0359a070aa75aefc80a25b091222e&units=metric&cnt=1";
+  var params = "id=524901&APPID=a5e0359a070aa75aefc80a25b091222e&units=metric&cnt=2";
   var url = "http://api.openweathermap.org/data/2.5/forecast/daily?"+params;
   getTemperature = function(callback){
     $.get(url, function(data){
-      var temps = data.list[0].temp;
+      var temps = data.list[1].temp;
       var averageTemp = Math.round((temps.min+temps.max)/2);
       callback(averageTemp);
     });
